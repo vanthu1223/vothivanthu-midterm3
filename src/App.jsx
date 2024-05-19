e6c5d00f00a6fb5d7e5e4cec28fb91771c542259
thu <thu.vo25@student.passerellesnumeriques.org>
2024-05-19 22:43:59 +0700

    Create basic web

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import "./App.css";
+import Navbar from "./components/layout/Navbar";
+import axios from "axios";
+import { useEffect } from "react";
+function App() {
+  useEffect( () => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("https://api.github.com/users");
+        console.log("GitHub Users: ", response.data);
+      }
+      catch (error) {
+        console.error("Error fetching data: ",error);
+      }
+    };
+    fetchData();
+  },[]);
+  return (
+    <div className="App">
+      <Navbar/>
+      <div className="container">
+        <h1>Hello from React</h1>
+      </div>
+    </div>
+  )
+}
+export default App;
